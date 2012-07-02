3aeec5e9db0572af022af6313738021c9f95df54
Joel <jnfeinstein@gmail.com>
2012-07-02 01:21:58 -0700

    Going through ruby blog tutorial to learn the system

---
+++ blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '3f65f01b8a39e2e0139075dcc64ac12293657944a557d6f93aa8781aa70e20e99cbf1f0a8e2980bdfd193f89b44e9c7e5386611ab72236cc7f7baab77cacf6a0'
