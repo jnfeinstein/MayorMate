a6fbe20e19e6b7c572546bda714cc824b98208b4
Joel <jnfeinstein@gmail.com>
2012-07-06 00:17:44 -0700

    Actually fetching access token now

--- website/app/controllers/access_codes_controller.rb
+++ website/app/controllers/access_codes_controller.rb
@@ -1,14 +1,17 @@
 class AccessCodesController < ApplicationController
   
+  CallbackUrl = "http://localhost:3000/access_codes/new".freeze
+  OAuthID = "YTRDZFMWBREGWX4MUUAKUPFDZXU3TUVIWZ3HQY3UGITW1K3Y".freeze
+  OAuthSecret = "AKBXY1M3PKL05TR0BHO1BE34W0U5OCWU54OOZZGU5503TMK0".freeze
   def index
-    foursquare = Foursquare::Base.new(
-    "YTRDZFMWBREGWX4MUUAKUPFDZXU3TUVIWZ3HQY3UGITW1K3Y", 
-    "AKBXY1M3PKL05TR0BHO1BE34W0U5OCWU54OOZZGU5503TMK0")
-    @fsq_link = foursquare.authorize_url("http://localhost:3000/access_codes/new")
+    foursquare = Foursquare::Base.new(OAuthID,OAuthSecret)
+    @fsq_link = foursquare.authorize_url(CallbackUrl)
   end
   
   def new
-    current_user.access_code = AccessCode.new(:code => params[:code])
+    foursquare = Foursquare::Base.new(OAuthID,OAuthSecret)
+    access_token = foursquare.access_token(params[:code], CallbackUrl)
+    current_user.access_code = AccessCode.new(:code => access_token)
     redirect_to root_url
   end
   
