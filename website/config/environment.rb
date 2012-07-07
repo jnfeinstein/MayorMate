372e0590345ee8c53e7747b04923b328d870a8c5
Joel <jnfeinstein@gmail.com>
2012-07-07 03:05:53 -0700

    Working on init

--- website/config/environment.rb
+++ website/config/environment.rb
@@ -3,7 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Website::Application.initialize!
-
-config.after_initialize do
-  CheckinsHelper.schedule_all_checkins
-end
