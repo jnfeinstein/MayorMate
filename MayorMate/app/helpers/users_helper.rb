353f32ab6b29ce60554e7cbdb985e1b7f65d6c52
Joel <jnfeinstein@gmail.com>
2012-07-08 05:16:03 -0700

    Storing user id rather than whole model

--- MayorMate/app/helpers/users_helper.rb
+++ MayorMate/app/helpers/users_helper.rb
@@ -1,11 +1,11 @@
 module UsersHelper
   
   def set_user(this_user)
-    session[:user] = this_user
+    session[:user] = this_user.id
   end
   
   def get_user
-    return session[:user]
+    return User.find(session[:user])
   end
   
 end
